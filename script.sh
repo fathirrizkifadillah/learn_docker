4698bad2b87427bddeae1af31e4b08828de86840
Fathir Rizki Fadillah <fathirrf80@gmail.com>
2026-08-19 22:11:41 +0700

    from and run + script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+# FROM Instruction
+
+docker build -t fathirrzkii/belajar-docker from
+
+docker image ls
+
+# RUN Instruction
+
+docker build -t fathirrzkii/belajar-docker:run run
+
+docker build -t fathirrzkii/belajar-docker:run run --progress=plain --no-cache
