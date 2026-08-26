14d84d9436f68024257b1fc2f44f7f922994c5ea
Fathir Rizki Fadillah <fathirrf80@gmail.com>
2026-08-26 23:06:28 +0700

    Services

---
+++ DOCKER COMPOSE/script.sh
@@ -0,0 +1,11 @@
+# Untuk membuat container dari docker compose (harus masuk ke folder yang ada docker composenya)
+docker compose create
+
+# Untuk menjalankan container
+docker compose start
+
+# Untuk menghentikan container
+docker compose stop
+
+# Untuk menghapus container yang ada di docker compose
+docker compose down
